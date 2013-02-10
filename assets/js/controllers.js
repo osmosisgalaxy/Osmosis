2c4f76f465f05f460d456ed686c5f50f71712be6
pohyz <pohyz@hotmail.com>
2013-02-10 18:49:27 +0800

    update

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -151,7 +151,7 @@ function StudentCtrl($scope,$resource){
   };
 
   $scope.deleteTeam = function(){
-    var data = {'method':"delete_Team",
+    var data = {'method':"delete_team",
       'team_id': $scope.stud_team["teamid"]};
 
     $scope.Model.send(data, function(response){
